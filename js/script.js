6f1c22fcd37135315d043957c1312cd92c7c613b
jcortes <jacortesmahmud@gmail.com>
2015-03-09 13:39:52 -0500

    Additional challenge

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 angular.module('myApp', [])
 
 .controller('myCtrl', function ($scope) {
+    $scope.gender = 'female';
     $scope.showText = false;
     $scope.word = {};
+    
+    $scope.sheOrHe = 'she';
+    $scope.herOrHis = 'her';
+    $scope.herOrHim = 'her';
+    $scope.genName = 'Female';
+    
+    $scope.changeGenTxt = function () {
+      if($scope.gender == 'female'){
+          $scope.sheOrHe = 'she';
+          $scope.herOrHis = 'her';
+          $scope.herOrHim = 'her';
+          $scope.genName = 'Female';
+      } else {
+          $scope.sheOrHe = 'he';
+          $scope.herOrHis = 'his';
+          $scope.herOrHim = 'him';
+          $scope.genName = 'Male';
+      }
+    }
 });
